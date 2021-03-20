29ea8870e79ca358eec12b9b5467d434468999b3
yzzeee <goooooodgood@gmail.com>
2021-03-20 11:36:52 +0900

    Express route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 //   "==============================> ",
 //   path.join(__dirname, "public/html")
 // );
+const createError = require("http-errors");
+const { zeroPlus, nowTime } = require("./modules/utils");
 
 app.listen(3000, () => {
   console.log("=====================");
@@ -30,7 +32,7 @@ app.use((req, res, next) => {
 
 app.get("/", function (req, res, next) {
   // 경로 위에 부터 순차적으로
-  console.log("", req.nowTime);
+  console.log("ddddddddddddddd", req.nowTime);
   res.send("<h1>Hello, Express</h1>");
 });
 
@@ -38,17 +40,33 @@ app.get("/", function (req, res, next) {
 // 익스프레스는 클라이언트로 부터 요청이 들어오면 위에서 부터 쭉 내려오면서 해석함
 
 app.get("/hello", (req, res, next) => {
-  console.log("", req.nowTime);
-  res.send(req.nowTime); // res가 있는 것들은 라우터라고 함!
+  console.log(req.query);
+  if (req.query.error) {
+    next({ code: 3333, msg: "테스트" });
+  } else {
+    console.log("", req.nowTime);
+    res.send(req.nowTime); // res가 있는 것들은 라우터라고 함!
+  }
 });
 
 // 이제부터 미들웨어를 만들어 보려고함~
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("Error Router");
   // 라우터 안의 콜백에는 인자가 최대 4개까지 가능함
-  res.send('<h1 style="margin: 100px; color: red;">Error 404</h1>');
+  const msg =
+    '<h1 style="margin: 100px; color: red;">Error 404</h1>' + nowTime();
+  next({ code: 404, msg }); // next()에 인자가 생기면 에러러 간주함
+  // next(createError(404, { msg, code: 404 }));
 });
 
-app.use((err, req, res) => {
+// 인자가 4개로 생각됨
+app.use((err, req, res, next) => {
   // 에러를 받아주는 라우터가 있다면 모든 라우터는 next라는 인자를 받을 수 있다.
+  // console.log(err.code, res.code, next.code, tets.code);
+  // res.json(`${err.code}</br>${error.msg}`);
+
+  console.log("Error Sender");
+  console.log(err.code);
+  res.send(err);
 });
